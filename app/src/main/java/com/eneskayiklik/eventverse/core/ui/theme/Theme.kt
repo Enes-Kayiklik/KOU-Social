f1799b94fdaf529a49637483ae4bc3c6a98390f1
Enes Kayiklik <eneskayiklik@gmail.com>
2022-04-12 22:24:31 +0300

    added animation on theme change

--- app/src/main/java/com/eneskayiklik/eventverse/core/ui/theme/Theme.kt
+++ app/src/main/java/com/eneskayiklik/eventverse/core/ui/theme/Theme.kt
@@ -1,13 +1,14 @@
 package com.eneskayiklik.eventverse.core.ui.theme
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+/*private val DarkColorPalette = darkColors(
     primary = Purple200,
     secondary = DarkSecondary,
     onSecondary = OnDarkSecondary,
@@ -34,15 +35,29 @@ private val LightColorPalette = lightColors(
     onBackground = Color.Black,
     onSurface = Color.Black,
     */
-)
+)*/
 
 @Composable
-fun EventverseTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme || isSystemInDarkTheme()) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun EventverseTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val spec = tween<Color>(
+        200, easing = LinearEasing
+    )
+    // Animate colors on theme change
+    val colors = Colors(
+        primary = Purple200,
+        onPrimary = White,
+        secondary = animateColorAsState(targetValue = if (darkTheme) DarkSecondary else Secondary, spec).value,
+        onSecondary = animateColorAsState(targetValue = if (darkTheme) OnDarkSecondary else OnSecondary, spec).value,
+        background = animateColorAsState(targetValue = if (darkTheme) Black100 else White, spec).value,
+        onBackground = animateColorAsState(targetValue = if (darkTheme) White else Black300, spec).value,
+        surface = animateColorAsState(targetValue = if (darkTheme) Black300 else BlueBackground, spec).value,
+        onSurface = OnSurface,
+        primaryVariant = Color(0xFF3700B3),
+        secondaryVariant = Color(0xFF018786),
+        error = if (darkTheme) Color(0xFFCF6679) else Color(0xFFB00020),
+        onError = White,
+        isLight = darkTheme.not()
+    )
 
     MaterialTheme(
         colors = colors,
