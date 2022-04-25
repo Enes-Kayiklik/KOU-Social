0e50e596238880859d95c0810dbde1261114525e
Enes Kayiklik <eneskayiklik@gmail.com>
2022-04-25 15:39:12 +0300

    updated like button anim

--- app/src/main/java/com/eneskayiklik/eventverse/feature/explore/component/SinglePostView.kt
+++ app/src/main/java/com/eneskayiklik/eventverse/feature/explore/component/SinglePostView.kt
@@ -1,12 +1,14 @@
 package com.eneskayiklik.eventverse.feature.explore.component
 
 import androidx.compose.animation.*
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
@@ -14,6 +16,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -25,6 +28,7 @@ import coil.request.ImageRequest
 import com.eneskayiklik.eventverse.data.model.share.Post
 import com.eneskayiklik.eventverse.feature.polls.component.poll_view.UserSection
 import com.eneskayiklik.eventverse.R
+import com.eneskayiklik.eventverse.core.ui.theme.Red
 
 @Composable
 fun SinglePostView(
@@ -48,7 +52,9 @@ fun SinglePostView(
         PostBody(
             body = post.body,
             image = post.image,
-            modifier = Modifier.padding(horizontal = 16.dp).padding(top = 5.dp)
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 5.dp)
         )
         PostInteractions(isLiked = post.isUserLike, likeCount = post.likeCount, onLike = onLike)
     }
@@ -98,23 +104,31 @@ private fun PostInteractions(
     onLike: () -> Unit
 ) {
     val colorAnim =
-        animateColorAsState(targetValue = if (isLiked.not()) MaterialTheme.colors.onBackground else MaterialTheme.colors.error).value
+        animateColorAsState(targetValue = if (isLiked) Red else MaterialTheme.colors.onBackground).value
+    val scaleAnim = animateFloatAsState(
+        targetValue = if (isLiked) 1.2F else 1F,
+        animationSpec = spring(Spring.DampingRatioHighBouncy, stiffness = 900F)
+    ).value
 
     Row(
         modifier = Modifier
-            .animateContentSize(),
+            .animateContentSize()
+            .padding(vertical = 10.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(5.dp)
     ) {
         Icon(
-            painter = painterResource(id = R.drawable.ic_like),
+            painter = painterResource(id = if (isLiked) R.drawable.ic_like_active else R.drawable.ic_like),
             contentDescription = null,
             tint = colorAnim,
-            modifier = Modifier.padding(start = 16.dp).clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple(bounded = false),
-                onClick = onLike
-            )
+            modifier = Modifier
+                .padding(start = 16.dp)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = rememberRipple(bounded = false),
+                    onClick = onLike
+                )
+                .scale(scaleAnim)
         )
         AnimatedContent(
             targetState = likeCount,
@@ -136,8 +150,17 @@ private fun PostInteractions(
                     style = MaterialTheme.typography.h4.copy(color = colorAnim, fontSize = 12.sp)
                 )
         }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(painter = painterResource(id = R.drawable.ic_reviews), contentDescription = null)
-        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_reviews),
+            contentDescription = null,
+            tint = MaterialTheme.colors.onBackground,
+            modifier = Modifier
+                .padding(horizontal = 5.dp)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = rememberRipple(bounded = false),
+                    onClick = onLike
+                )
+        )
     }
 }
