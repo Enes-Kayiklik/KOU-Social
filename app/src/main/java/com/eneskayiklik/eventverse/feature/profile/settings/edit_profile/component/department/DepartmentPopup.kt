2300adfd853aee9a9066c14cd5e32fd2b4e5e70f
Enes Kayiklik <eneskayiklik@gmail.com>
2022-05-12 12:35:58 +0300

    added max lines to DepartmentPopup.kt

--- app/src/main/java/com/eneskayiklik/eventverse/feature/profile/settings/edit_profile/component/department/DepartmentPopup.kt
+++ app/src/main/java/com/eneskayiklik/eventverse/feature/profile/settings/edit_profile/component/department/DepartmentPopup.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -66,6 +67,8 @@ fun DepartmentPopup(
                                     fontSize = 16.sp
                                 ),
                                 textAlign = TextAlign.Start,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
                                 modifier = Modifier
                                     .fillMaxWidth()
                                     .clickable {
